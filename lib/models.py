8702771d341ac8bdc407c52e4c6b0c7c5b6f1c8d
phil-ogutu <phil.shifwoko@gmail.com>
2025-05-25 22:32:09 +0300

    migrations and seeds

--- lib/models.py
+++ lib/models.py
@@ -16,14 +16,53 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship("Freebie", back_populates="company") 
+    
     def __repr__(self):
         return f'<Company {self.name}>'
 
+    def give_freebie(self, dev, item_name, value):
+        return Freebie(item_name=item_name, value=value, company=self, dev=dev)
+    
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
+
+    
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    freebies = relationship("Freebie", back_populates="dev") 
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+    @property
+    def companies(self):
+        return list({freebie.company for freebie in self.freebies})
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = dev   
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    company_id = Column(Integer, ForeignKey('companies.id'))
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+
+    company = relationship("Company", back_populates="freebies")
+    dev = relationship("Dev", back_populates="freebies")
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
+
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
+from sqlalchemy import create_engine
 
 # Script goes here!
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.query(Freebie).delete()
+session.query(Company).delete()
+session.query(Dev).delete()
+
+dev1 = Dev(name="Alice")
+dev2 = Dev(name="Bob")
+dev3 = Dev(name="John")
+
+company1 = Company(name="Centrino", founding_year=2012)
+company2 = Company(name="Google", founding_year=1998)
+
+freebie1 = Freebie(item_name="T-shirt", value=10, company=company1, dev=dev1)
+freebie2 = Freebie(item_name="Sticker", value=1, company=company1, dev=dev2)
+freebie3 = Freebie(item_name="Mug", value=15, company=company2, dev=dev1)
+
+session.add_all([dev1, dev2, dev3, company1, company2, freebie1, freebie2, freebie3])
+session.commit()
